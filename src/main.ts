ae0d64ca8c8fa02e598dd7cede08f831f18551de
qianwen <2874765209@qq.com>
2022-03-20 22:34:43 +0800

    style(login): project init

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+// 对路由进行注册
+import router from './router'
 
-createApp(App).mount("#app");
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
